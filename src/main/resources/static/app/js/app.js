1a9246f382725c40cbc28c6b6d1312c0c3e4ffb2
redStudio <gzxyong@126.com>
2015-09-26 11:11:23 +0800

    增加路由功能

--- src/main/resources/static/app/js/app.js
+++ src/main/resources/static/app/js/app.js
@@ -1,9 +1,26 @@
 'use strict';
 
 /* App Module */
-(function(angular) {
-	angular.module("myApp.controllers", []);
-	angular.module("myApp.services", []);
-	angular.module("myApp", [ "ngResource", "myApp.controllers",
-			"myApp.services" ]);
-}(angular));
+/*
+ * (function(angular) { angular.module("myApp.controllers", []);
+ * angular.module("myApp.services", []); angular.module("myApp", [ "ngResource",
+ * "myApp.controllers", "myApp.services" ]);
+ * 
+ * 
+ * 
+ * }(angular));
+ */
+
+var myApp = angular.module('myApp', [ 'ngRoute', 'userControllers' ]);
+
+myApp.config([ '$routeProvider', 
+               function($routeProvider) {
+	$routeProvider.
+	when('/users', {
+		templateUrl : 'app/view/user/user-list.html',
+		controller : 'UserListCtrl'}).
+	when('/users/:id', {
+		templateUrl : 'app/view/user/user-detail.html',
+		controller : 'UserDetailCtrl'	}).
+	otherwise({redirectTo: '/users');
+} ]);
--- src/main/resources/static/app/js/controllers.js
+++ src/main/resources/static/app/js/controllers.js
@@ -1,6 +1,25 @@
 'use strict';
 
-(function(angular) {
+var userControllers = angular.module('userControllers', []);
+
+userControllers.controller('UserListCtrl', ['$scope', '$http',
+  function($scope, $http) {
+    $http.get('phones/phones.json').success(function(data) {
+      $scope.phones = data;
+    });
+
+    $scope.orderProp = 'age';
+  }]);
+
+userControllers.controller('UserDetailCtrl', ['$scope', '$routeParams', '$http',
+  function($scope, $routeParams, $http) {
+    $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
+      $scope.phone = data;
+    });
+  }]);
+
+
+/*(function(angular) {
 	var AppController = function($scope, Item) {
 		Item.query(function(response) {
 			$scope.items = response ? response : [];
@@ -34,4 +53,4 @@
 	AppController.$inject = [ '$scope', 'Item' ];
 	angular.module("myApp.controllers").controller("AppController",
 			AppController);
-}(angular));
+}(angular));*/
